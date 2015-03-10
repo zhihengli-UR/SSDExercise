dce1645815e47fbb995fcb78cc29b1a76faeb435
hubertlee915 <hubertlee915@gmail.com>
2015-03-10 22:26:18 +0800

    Initial Commit

---
+++ SSDExercise/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SSDExercise
+//
+//  Created by 李 智恒 on 15/3/10.
+//  Copyright (c) 2015年 李 智恒. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
